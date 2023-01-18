b388086b2521a23d55c4514f21db922bf0770a65
d3d9-dll <116770928+d3d9-dll@users.noreply.github.com>
2023-01-18 22:37:24 +0300

    Fixing block shape

--- src/main/java/github/d3d9_dll/minecraft/fabric/erek/block/SlotMachineBottomCaseBlock.java
+++ src/main/java/github/d3d9_dll/minecraft/fabric/erek/block/SlotMachineBottomCaseBlock.java
@@ -3,11 +3,16 @@ package github.d3d9_dll.minecraft.fabric.erek.block;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tools.FabricToolTags;
 import net.minecraft.block.*;
+import net.minecraft.entity.EntityContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class SlotMachineBottomCaseBlock extends HorizontalFacingBlock {
 
@@ -16,6 +21,27 @@ public class SlotMachineBottomCaseBlock extends HorizontalFacingBlock {
             FabricBlockSettings.of(Material.METAL)
                     .strength(6f)
                     .breakByTool(FabricToolTags.PICKAXES, 2)
+    );
+
+    private final VoxelShape SHAPE_NORTH = VoxelShapes.union(
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
+
+            Block.createCuboidShape(1.0D, 14.0D, 0.0D, 15.0D, 16.0D, 2.0D)
+    );
+    private final VoxelShape SHAPE_EAST = VoxelShapes.union(
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
+
+            Block.createCuboidShape(14.0D, 14.0D, 1.0D, 16.0D, 16.0D, 15.0D)
+    );
+    private final VoxelShape SHAPE_SOUTH = VoxelShapes.union(
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
+
+            Block.createCuboidShape(1.0D, 14.0D, 14.0D, 15.0D, 16.0D, 16.0D)
+    );
+    private final VoxelShape SHAPE_WEST = VoxelShapes.union(
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
+
+            Block.createCuboidShape(0.0D, 14.0D, 1.0D, 2.0D, 16.0D, 15.0D)
     );
 
     public SlotMachineBottomCaseBlock(Block.Settings settings) {
@@ -31,6 +57,33 @@ public class SlotMachineBottomCaseBlock extends HorizontalFacingBlock {
     @Override
     public void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(FACING);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, EntityContext context) {
+        return getShape(state);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView view, BlockPos pos, EntityContext context) {
+        return getShape(state);
+    }
+
+    private VoxelShape getShape(BlockState state){
+        Direction dir = state.get(FACING);
+        switch(dir) {
+            default:
+            case NORTH:
+                return SHAPE_NORTH;
+            case SOUTH:
+                return SHAPE_SOUTH;
+            case EAST:
+                return SHAPE_EAST;
+            case WEST:
+                return SHAPE_WEST;
+        }
     }
 
     @SuppressWarnings("deprecation")
