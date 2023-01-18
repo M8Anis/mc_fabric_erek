e331314b845aab1e2fed06c9f1762aa4c9a75411
d3d9-dll <116770928+d3d9-dll@users.noreply.github.com>
2023-01-18 22:37:26 +0300

    Adding TODO list Changing packet names

--- src/main/java/github/d3d9_dll/minecraft/fabric/erek/Entrypoint.java
+++ src/main/java/github/d3d9_dll/minecraft/fabric/erek/Entrypoint.java
@@ -19,11 +19,11 @@ public class Entrypoint implements ModInitializer {
     public final static Logger LOGGER = LogManager.getLogger("d3d9_dll EREK");
 
     public static final Identifier PACKET_SLOTMACHINE_SPIN =
-            new Identifier("d3d9_dllerek", "packet_slotmachine_spin");
+            new Identifier("d3d9_dllerek", "packet_d3d9_dllerek_slotmachine_spin");
     public static final Identifier PACKET_SLOTMACHINE_BALANCE =
-            new Identifier("d3d9_dllerek", "packet_slotmachine_balance");
+            new Identifier("d3d9_dllerek", "packet_d3d9_dllerek_slotmachine_balance");
     public static final Identifier PACKET_VERSION_SYNC =
-            new Identifier("d3d9_dllerek", "packet_version_synchronize");
+            new Identifier("d3d9_dllerek", "packet_d3d9_dllerek_version_synchronize");
 
     public static final ItemGroup GENERAL_ITEM_GROUP = FabricItemGroupBuilder.create(
                     new Identifier("d3d9_dllerek", "casino"))
