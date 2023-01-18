8117db3c5bec9f8b55d03267faf65970c0f604df
d3d9-dll <116770928+d3d9-dll@users.noreply.github.com>
2023-01-18 22:37:29 +0300

    Supply warnings

--- src/main/java/github/d3d9_dll/minecraft/fabric/erek/Version.java
+++ src/main/java/github/d3d9_dll/minecraft/fabric/erek/Version.java
@@ -5,6 +5,7 @@ public class Version {
     public static final int IS_DEBUG = 1;
     public static final int VERSION_RELEASE_TYPE = 0x1A_A19A;// 0x1A_A19A (Alpha), 0x2B_BE7A (Beta), 0x3C_91E1 (Release)
     public static final int VERSION_NUMBER = 6;
+    @SuppressWarnings({"PointlessArithmeticExpression", "RedundantSuppression"})
     public static final long VERSION_FINGERPRINT =
             ((long)VERSION_RELEASE_TYPE << 32) + (IS_DEBUG << 30) + VERSION_NUMBER;
 
