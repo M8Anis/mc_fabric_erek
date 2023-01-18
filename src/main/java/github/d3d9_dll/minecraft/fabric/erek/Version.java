f543d79551a909f2a03808fbb3f9987401bff594
d3d9-dll <116770928+d3d9-dll@users.noreply.github.com>
2023-01-18 22:37:34 +0300

    Release 7-alpha-dev

--- src/main/java/github/d3d9_dll/minecraft/fabric/erek/Version.java
+++ src/main/java/github/d3d9_dll/minecraft/fabric/erek/Version.java
@@ -2,7 +2,7 @@ package github.d3d9_dll.minecraft.fabric.erek;
 
 public class Version {
 
-    public static final int IS_DEBUG = 1;
+    public static final int IS_DEBUG = 0;
     public static final int VERSION_RELEASE_TYPE = 0x1A_A19A;// 0x1A_A19A (Alpha), 0x2B_BE7A (Beta), 0x3C_91E1 (Release)
     public static final int VERSION_NUMBER = 7;
     @SuppressWarnings({"PointlessArithmeticExpression", "RedundantSuppression"})
