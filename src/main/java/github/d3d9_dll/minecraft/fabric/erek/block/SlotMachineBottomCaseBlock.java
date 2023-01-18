0af188712df45bc1afa6da432eee43fdca64fda1
d3d9-dll <116770928+d3d9-dll@users.noreply.github.com>
2023-01-18 22:37:30 +0300

    Fixing empty space in the SlotMachineBottomCaseBlock

--- src/main/java/github/d3d9_dll/minecraft/fabric/erek/block/SlotMachineBottomCaseBlock.java
+++ src/main/java/github/d3d9_dll/minecraft/fabric/erek/block/SlotMachineBottomCaseBlock.java
@@ -26,22 +26,30 @@ public class SlotMachineBottomCaseBlock extends HorizontalFacingBlock {
     private final VoxelShape SHAPE_NORTH = VoxelShapes.union(
             Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
 
-            Block.createCuboidShape(1.0D, 14.0D, 0.0D, 15.0D, 16.0D, 2.0D)
+            Block.createCuboidShape(1.0D, 14.0D, 0.0D, 15.0D, 16.0D, 2.0D),
+
+            Block.createCuboidShape(1.0D, 14.0D, 1.0D, 15.0D, 16.0D, 15.0D)
     );
     private final VoxelShape SHAPE_EAST = VoxelShapes.union(
             Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
 
-            Block.createCuboidShape(14.0D, 14.0D, 1.0D, 16.0D, 16.0D, 15.0D)
+            Block.createCuboidShape(14.0D, 14.0D, 1.0D, 16.0D, 16.0D, 15.0D),
+
+            Block.createCuboidShape(1.0D, 14.0D, 1.0D, 15.0D, 16.0D, 15.0D)
     );
     private final VoxelShape SHAPE_SOUTH = VoxelShapes.union(
             Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
 
-            Block.createCuboidShape(1.0D, 14.0D, 14.0D, 15.0D, 16.0D, 16.0D)
+            Block.createCuboidShape(1.0D, 14.0D, 14.0D, 15.0D, 16.0D, 16.0D),
+
+            Block.createCuboidShape(1.0D, 14.0D, 1.0D, 15.0D, 16.0D, 15.0D)
     );
     private final VoxelShape SHAPE_WEST = VoxelShapes.union(
             Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
 
-            Block.createCuboidShape(0.0D, 14.0D, 1.0D, 2.0D, 16.0D, 15.0D)
+            Block.createCuboidShape(0.0D, 14.0D, 1.0D, 2.0D, 16.0D, 15.0D),
+
+            Block.createCuboidShape(1.0D, 14.0D, 1.0D, 15.0D, 16.0D, 15.0D)
     );
 
     public SlotMachineBottomCaseBlock(Block.Settings settings) {
