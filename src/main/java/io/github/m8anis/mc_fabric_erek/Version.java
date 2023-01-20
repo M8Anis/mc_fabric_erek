6ab4df728be5c4e4190b84a310ee91c342639ae3
M8Anis <116770928+M8Anis@users.noreply.github.com>
2023-01-21 01:32:48 +0300

    Increasing version after release

--- src/main/java/io/github/m8anis/mc_fabric_erek/Version.java
+++ src/main/java/io/github/m8anis/mc_fabric_erek/Version.java
@@ -24,9 +24,9 @@ package io.github.m8anis.mc_fabric_erek;
 
 public class Version {
 
-    public static final int IS_DEBUG = 0;
+    public static final int IS_DEBUG = 1;
     public static final int VERSION_RELEASE_TYPE = 0x1A_A19A;// 0x1A_A19A (Alpha), 0x2B_BE7A (Beta), 0x3C_91E1 (Release)
-    public static final int VERSION_NUMBER = 9;
+    public static final int VERSION_NUMBER = 10;
     @SuppressWarnings({"PointlessArithmeticExpression", "RedundantSuppression"})
     public static final long VERSION_SERIAL = ((long) VERSION_RELEASE_TYPE << 32) + (IS_DEBUG << 30) + VERSION_NUMBER;
 
